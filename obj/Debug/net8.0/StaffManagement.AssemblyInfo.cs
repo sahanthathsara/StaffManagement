51f48fa961d372d164428b64d402bfeb80578349
Sahan Thathsara <sahanthathsara14@gmail.com>
2026-02-23 22:01:00 +0530

    HR changes

--- obj/Debug/net8.0/StaffManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/StaffManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaffManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1ad1878c78905210be52e03c7dc640238e6d39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3fbe0406a07cfbace99c0c77d858e520b3a78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaffManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaffManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
